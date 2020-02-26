c0043515f9b56a7bfa80d851db6b1dfd0bdfcbd1
Manjunath .M <59873177+mreddy89@users.noreply.github.com>
2020-02-26 16:17:27 +0400

    removed python

--- python_scripts/ec2_scripts/ec2_ID_state_describe.py
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env python
-import boto3
-ec2 = boto3.resource('ec2')
-for instance in ec2.instances.all():
-    print (instance.id, instance.state, instance.platform)
--- python_scripts/ec2_scripts/s3_list.py
+++
@@ -1,9 +0,0 @@
-import boto3
-import datetime
-
-s3 = boto3.client('s3')
-response = s3.list_buckets()
-
-for bucket in response["Buckets"]:
-    print(bucket["Name"], bucket["CreationDate"])
-#print (response)

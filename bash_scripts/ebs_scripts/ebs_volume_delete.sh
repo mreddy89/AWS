28714ddb926eeb7282970f3c7602c0f8a43d4b47
Manjunath .M <59873177+mreddy89@users.noreply.github.com>
2020-02-26 15:48:02 +0400

    Contains bash scripts

---
+++ bash_scripts/ebs_scripts/ebs_volume_delete.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+region=eu-west-1
+profile=test
+input="./ebs_list.txt"
+
+#while read line;
+while IFS= read -r line
+do
+	volumeid=${line}
+	#datetime=$(echo "$(date +'%d-%m-%Y_%H:%M')")
+	#echo "Instance ID is : ${ec2id}";
+	# this is to fetch the volumeid, size and state of the EBS volumes for the above mentioned account and volume IDs mentioned in ebs_list.txt file
+	result=$(aws --profile ${profile} --region ${region} ec2 describe-volumes --volume-id ${volumeid} --query 'Volumes[].[VolumeId,Size,State]' --output text)
+echo ${result}
+	result1=$(aws --profile ${profile} --region ${region} ec2 delete-volume --volume-id ${volumeid})
+	echo "Deleted the volume : ${volumeid}"
+done < $input
